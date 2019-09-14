b14ea2dc83da6bcef363008e2dc0def2325da4d2
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-14 13:28:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each do | squared |
-    new_array = squared** 2 
+  array.each do | value |
+    new_array = squared ** 2 
   end
   new_array
   
